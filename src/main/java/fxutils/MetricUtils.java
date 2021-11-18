557c052e87a6147fcd3f04137163072d4e90590e
RealThanhpv <fireinwinterfell@gmail.com>
2021-11-18 08:38:12 +0700

    update MetricUtils

--- src/main/java/fxutils/MetricUtils.java
+++ src/main/java/fxutils/MetricUtils.java
@@ -2,11 +2,11 @@ package fxutils;
 
 public class MetricUtils {
 
-    static public double pointToPixel(double points){
+    static public double pointsToPixels(double points){
         return points*0.75;
     }
 
-    static public double pixelsToPoint(double pixels){
+    static public double pixelsToPoints(double pixels){
         return pixels/0.75;
     }
 }
